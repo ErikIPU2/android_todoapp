8f6e2f7d92da5dacf3253d4a3fc00834fec9b34e
Erik <erikborella@gmail.com>
2019-08-11 16:43:53 -0300

    added inputs check to create account

--- app/src/main/java/com/erik/todoapp/activities/CreateAccountActivity.kt
+++ app/src/main/java/com/erik/todoapp/activities/CreateAccountActivity.kt
@@ -2,6 +2,7 @@ package com.erik.todoapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import com.erik.todoapp.R
@@ -43,5 +44,69 @@ class CreateAccountActivity : AppCompatActivity() {
 
             findViewById(R.id.button_create_account_create)
         )
+
+        viewHolder.createButton.setOnClickListener(this.createButtonOnClick)
+    }
+
+    private val createButtonOnClick = View.OnClickListener { view ->
+        if (this.checkInputs()) {
+            this.checkPasswords()
+        }
+    }
+
+    private fun checkInputs():Boolean {
+        val completeName:String = viewHolder.completeNameTextInputEditText.text.toString()
+        val email:String = viewHolder.emailTextInputEditText.text.toString()
+        val password:String = viewHolder.passwordTextInputEditText.text.toString()
+        val confirmPassword:String = viewHolder.confirmPasswordTextInputEditText.text.toString()
+
+        var isOkay = true
+
+        if (completeName.isEmpty()) {
+            viewHolder.completeNameTextInputLayout.error = getString(
+                R.string.voce_precisa_digitar_o_seu_nome)
+            isOkay = false
+        } else {
+            viewHolder.completeNameTextInputLayout.error = null
+        }
+
+        if (email.isEmpty()) {
+            viewHolder.emailTextInputLayout.error = getString(R.string.voce_precisa_digitar_o_email)
+            isOkay = false
+        } else {
+            viewHolder.emailTextInputLayout.error = null
+        }
+
+        if (password.isEmpty()) {
+            viewHolder.passwordTextInputLayout.error = getString(
+                R.string.voce_precisa_digitar_a_senha)
+            isOkay = false
+        } else {
+            viewHolder.passwordTextInputLayout.error = null
+        }
+
+        if (confirmPassword.isEmpty()) {
+            viewHolder.confirmPasswordTextInputLayout.error = getString(
+                R.string.voce_precisa_confirmar_a_senha)
+            isOkay = false
+        } else {
+            viewHolder.confirmPasswordTextInputLayout.error = null
+        }
+
+        return isOkay
+    }
+
+    private fun checkPasswords():Boolean {
+        val password:String = viewHolder.passwordTextInputEditText.text.toString()
+        val confirmPassword:String = viewHolder.confirmPasswordTextInputEditText.text.toString()
+
+        if (password != confirmPassword) {
+            viewHolder.confirmPasswordTextInputLayout.error = getString(
+                R.string.as_senhas_nao_sao_iguais)
+            return false
+        } else {
+            viewHolder.confirmPasswordTextInputLayout.error = null
+            return true
+        }
     }
 }
