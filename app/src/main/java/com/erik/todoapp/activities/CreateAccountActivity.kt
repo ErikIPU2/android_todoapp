51bdc20490bbdd7031a060a002acf65c80e5e0d9
Erik <erikborella@gmail.com>
2019-08-11 16:21:29 -0300

    create account activity changes
    EditText's change to TextInput
    added ScroolView

--- app/src/main/java/com/erik/todoapp/activities/CreateAccountActivity.kt
+++ app/src/main/java/com/erik/todoapp/activities/CreateAccountActivity.kt
@@ -5,14 +5,21 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import com.erik.todoapp.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class CreateAccountActivity : AppCompatActivity() {
 
     private data class ViewHolder(
-            val completeNameEditText: EditText,
-            val emailEditText: EditText,
-            val passwordEditText: EditText,
-            val confirmPasswordEditText: EditText,
+            val completeNameTextInputLayout: TextInputLayout,
+            val emailTextInputLayout: TextInputLayout,
+            val passwordTextInputLayout: TextInputLayout,
+            val confirmPasswordTextInputLayout: TextInputLayout,
+
+            val completeNameTextInputEditText: TextInputEditText,
+            val emailTextInputEditText: TextInputEditText,
+            val passwordTextInputEditText: TextInputEditText,
+            val confirmPasswordTextInputEditText: TextInputEditText,
 
             val createButton: Button
     )
@@ -24,11 +31,17 @@ class CreateAccountActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_account)
 
         this.viewHolder = ViewHolder(
-                findViewById(R.id.edit_createaccount_completename),
-                findViewById(R.id.edit_createaccount_email),
-                findViewById(R.id.edit_createaccount_password),
-                findViewById(R.id.edit_createaccount_confirmpassword),
-                findViewById(R.id.button_createaccount_create)
+            findViewById(R.id.text_input_layout_create_account_complete_name),
+            findViewById(R.id.text_input_layout_create_account_email),
+            findViewById(R.id.text_input_layout_create_account_password),
+            findViewById(R.id.text_input_layout_create_account_confirm_password),
+
+            findViewById(R.id.text_input_edit_create_account_complete_name),
+            findViewById(R.id.text_input_edit_create_account_email),
+            findViewById(R.id.text_input_edit_create_account_password),
+            findViewById(R.id.text_input_edit_create_account_confirm_password),
+
+            findViewById(R.id.button_create_account_create)
         )
     }
 }
