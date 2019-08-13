e24b1164354f07f3df35cc6d8806a4db6436d3b8
Erik <erikborella@gmail.com>
2019-08-13 07:07:50 -0300

    create viewholder for create_todo

--- app/src/main/java/com/erik/todoapp/activities/CreateTodoActivity.kt
+++ app/src/main/java/com/erik/todoapp/activities/CreateTodoActivity.kt
@@ -2,12 +2,28 @@ package com.erik.todoapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.widget.Toolbar
 import com.erik.todoapp.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class CreateTodoActivity : AppCompatActivity() {
 
     private data class ViewHolder(
+        val titleTextInputLayout: TextInputLayout,
+        val dataTextInputLayout: TextInputLayout,
+
+        val titleTextInputEditText: TextInputEditText,
+        val dataTextInputEditText: TextInputEditText,
+
+        val priorityRadioGroup: RadioGroup,
+
+        val highRadioButton: RadioButton,
+        val mediumRadioButton: RadioButton,
+        val lowRadioButton: RadioButton,
+
         val toolbar: Toolbar
     )
 
@@ -18,11 +34,25 @@ class CreateTodoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_todo)
 
         this.viewHolder = ViewHolder(
+            findViewById(R.id.text_input_layout_create_todo_title),
+            findViewById(R.id.text_input_layout_create_todo_data),
+
+            findViewById(R.id.text_input_edit_create_todo_title),
+            findViewById(R.id.text_input_edit_create_todo_data),
+
+            findViewById(R.id.radio_group_create_todo_priority),
+
+            findViewById(R.id.radio_button_create_todo_high),
+            findViewById(R.id.radio_button_create_todo_medium),
+            findViewById(R.id.radio_button_create_todo_low),
+
             findViewById(R.id.toolbar_create_todo_toolbar)
         )
 
         setSupportActionBar(viewHolder.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        viewHolder.lowRadioButton.isChecked = true
 
     }
 
