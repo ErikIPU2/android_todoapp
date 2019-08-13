38db6b00f4d68abd379113a24b690195c12a8c73
Erik <erikborella@gmail.com>
2019-08-13 08:24:11 -0300

    Added new method to dataTonton

--- app/src/main/java/com/erik/todoapp/util/time/DataInvalidaException.java
+++ app/src/main/java/com/erik/todoapp/util/time/DataInvalidaException.java
@@ -1,4 +1,20 @@
 package com.erik.todoapp.util.time;
 
-public class DataInvalidaException {
+public class DataInvalidaException extends Exception {
+    /**
+     * Define a mensagem de erro
+     * @param message Mensagem de erro
+     */
+    public DataInvalidaException(String message) {
+        super(message);
+    }
+
+    /**
+     * Retorna a mensagem de erro
+     * @return Mensagem de erro
+     */
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
 }
