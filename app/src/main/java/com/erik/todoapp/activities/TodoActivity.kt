b0d8cb94b10ead3522f6a7c012823e058070d350
Erik <erikborella@gmail.com>
2019-08-13 06:20:53 -0300

    added fab and coordinator layout

--- app/src/main/java/com/erik/todoapp/activities/TodoActivity.kt
+++ app/src/main/java/com/erik/todoapp/activities/TodoActivity.kt
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.erik.todoapp.R
 import com.erik.todoapp.util.Session
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TodoActivity : AppCompatActivity() {
 
     private data class ViewHolder(
-        val toolbar: Toolbar
+        val toolbar: Toolbar,
+        val addTodoFloatingActionButton: FloatingActionButton
     )
 
     private lateinit var viewHolder: ViewHolder
@@ -24,10 +28,12 @@ class TodoActivity : AppCompatActivity() {
         this.session = Session(this)
 
         this.viewHolder = ViewHolder(
-            findViewById(R.id.toolbar_todo_toolbar)
+            findViewById(R.id.toolbar_todo_toolbar),
+            findViewById(R.id.fab_todo_add_todo)
         )
 
         setSupportActionBar(viewHolder.toolbar)
+        viewHolder.addTodoFloatingActionButton.setOnClickListener(this.fabAddTodoOnClick)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -43,6 +49,10 @@ class TodoActivity : AppCompatActivity() {
         } else -> {
             super.onOptionsItemSelected(item)
         }
+    }
+
+    private val fabAddTodoOnClick = View.OnClickListener { view ->
+        Toast.makeText(this, "Tu APERTOU NO FAB MANOOOOOOOOOOOOOOOOOOOOO", Toast.LENGTH_SHORT).show()
     }
 
     private fun close() {
